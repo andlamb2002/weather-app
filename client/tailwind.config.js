b9a4ddc31d2bfa8f946140eb4b6743daf873c10d
Andreas Lambropoulos <andlamb2002@gmail.com>
2024-04-03 13:12:50 -0400

    Setup colors and font

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -6,18 +6,18 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        // primary: "#A8E7EF",
-        // secondary: "#FFD700",
-        // textColor: "#003865",
-        // header: "#007BFF",
-        // lightRed: "#FFB8B8",
-        // white: "#FFFFFF",
-        // black: "#000000",
-        // accent: "#C2790A",
-        // accentRed: "#FF4D4D",
+        bg: "#EBEFEF",
+        text: '#2D2F34',
+        header: '#3A91C4',
+        darkBg: '#1A3F56',
+        white: '#FFFFFF',
+        whiteHover: '#F7F7F7',
+        black: "#000000",
+        orange: '#FFCF99',
+        orangeHover: '#FFB561', 
       },
       fontFamily: {
-        // sans: ['Lato', 'sans-serif'],
+        sans: ['"Open Sans"', 'sans-serif'],
       },
     },
   },
